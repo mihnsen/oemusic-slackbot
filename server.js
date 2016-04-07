13a1fcd4f8259a151851f0f67de42e57a086d147
minhnt <minhnt@ownego.com>
2016-04-07 12:03:54 +0700

    Oe musical

---
+++ server.js
@@ -0,0 +1,58 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var webdriver = require('selenium-webdriver');
+
+var app = express();
+var port = process.env.PORT || 3456;
+
+// body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var driver = new webdriver.Builder().
+     withCapabilities(webdriver.Capabilities.chrome()).
+     build();
+function play(url) {
+  driver.get(url);
+}
+
+function stop() {
+  driver.quit();
+}
+
+app.post('/music', function(req, res, next) {
+  var commands = req.body.text.split(' ');
+  var user = req.body.user_name;
+
+  var action = commands[0];
+  var url;
+
+  if(commands.length > 1) url = commands[1];
+  var botPayload = {
+    text : 'Playing ' + url + ' by ' + user
+  };
+
+
+  if(action == 'play' && url != undefined) play(url);
+  if(action == 'stop' || action == 'pause') stop();
+
+  return res.status(200).json(botPayload);
+})
+
+
+
+
+// basic error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port '+port+'!');
+});
